bf451b17cd6a4020c8a68808ae5e723562fa7f1e
Ollie Etherington <ollie@etherington.xyz>
2024-11-23 18:35:19 +0000

    Add theming and branding for storybook

---
+++ .storybook/theme.js
@@ -0,0 +1,9 @@
+import { create } from "@storybook/theming";
+
+export default create({
+	base: "dark",
+	brandTitle: "Bluesky Embed React",
+	brandUrl: "https://github.com/oetherington/bluesky-embed-react",
+	brandImage: "",
+	brandTarget: "_self",
+});
