a0ca55ce472a34a2c3fd2099e33e12b71a3098c7
Ollie Etherington <ollie@etherington.xyz>
2024-12-01 18:51:55 +0000

    Add branding icon

--- .storybook/theme.js
+++ .storybook/theme.js
@@ -1,8 +1,9 @@
 import { create } from "@storybook/theming";
 
 export default create({
+	base: "light",
 	brandTitle: "Bluesky Embed React",
 	brandUrl: "https://github.com/oetherington/bluesky-embed-react",
-	brandImage: "",
+	brandImage: "/favicon.svg",
 	brandTarget: "_self",
 });
