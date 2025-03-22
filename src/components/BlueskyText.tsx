ea6ffd7aa7f8cb555176ad12047e1e316d560d7b
Ollie Etherington <ollie@etherington.xyz>
2025-03-22 21:50:28 +0000

    Fix incorrect hrefs for posts with long links

--- src/components/BlueskyText.tsx
+++ src/components/BlueskyText.tsx
@@ -1,24 +1,35 @@
 import React, { FC, useEffect, useState } from "react";
-import { RichText, RichTextSegment } from "@atproto/api";
+import { Facet, RichText, RichTextSegment } from "@atproto/api";
 import { useBlueskyClient } from "../hooks/useBlueskyClient";
 import { useBlueskyConfig } from "../hooks/useBlueskyConfig";
 import { BlueskySegment } from "./BlueskySegment";
 
 export type BlueskyTextProps = {
 	text: string;
+	facets?: Facet[];
 };
 
-export const BlueskyText: FC<BlueskyTextProps> = ({ text }) => {
+export const BlueskyText: FC<BlueskyTextProps> = ({ text, facets }) => {
 	const client = useBlueskyClient();
 	const { textPrimaryColor, fontSize, fontWeight } = useBlueskyConfig();
-	const [segments, setSegments] = useState<RichTextSegment[]>([]);
+
+	const [segments, setSegments] = useState<RichTextSegment[]>(() => {
+		if (facets?.length) {
+			const rt = new RichText({ text, facets });
+			return Array.from(rt.segments());
+		} else {
+			return [];
+		}
+	});
 
 	useEffect(() => {
-		void (async () => {
-			const rt = new RichText({ text });
-			await rt.detectFacets(client);
-			setSegments(Array.from(rt.segments()));
-		})();
+		if (!facets?.length) {
+			void (async () => {
+				const rt = new RichText({ text, facets });
+				await rt.detectFacets(client);
+				setSegments(Array.from(rt.segments()));
+			})();
+		}
 	}, [text, client]);
 
 	return (
